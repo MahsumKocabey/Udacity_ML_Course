d9e01eb252621735be87b045c1fe1471728d7168
mahsumkocabey <mahsumkocabey@hotmail.com>
2021-03-28 22:41:02 +0100

    Run and predict GaussianNB from sklearn.naive_bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,26 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
 
 
 #########################################################
 ### your code goes here ###
-
+clt = GaussianNB()
+clt.fit(features_train, labels_train)
+clt.predict(features_test)
+print(clt.score(features_test, labels_test))
 
 #########################################################
-
-
